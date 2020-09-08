9d266de82fdb517a7c0e58a35fc0f2d3eddc4b9e
Nico Rithner <nicorithner@gmail.com>
2020-09-08 07:33:21 -0600

    Create features test for students index. Can see list of students with each name, age and house

---
+++ spec/features/students/index_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe "Students index page" do
+  before :each do
+    @student_1 = Student.create!(name: "Student 1", age: 10, house: "House A")
+    @student_2 = Student.create!(name: "Student 2", age: 10, house: "House A")
+    @student_3 = Student.create!(name: "Student 3", age: 10, house: "House B")
+  end
+
+  describe "Index page '/students' display students' list with  student's details" do
+    it "display list of students with details" do
+      visit "/students"
+
+      expect(page).to have_content("Student 1")
+
+      within("#student-#{@student_1.id}") do
+        expect(page).to have_content("Student 1")
+        expect(page).to have_content(10)
+        expect(page).to have_content("House A")
+      end
+      
+    end
+  end
+end
