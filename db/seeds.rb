6829e56471b81566393a008747b653cb4926fd33
Nico Rithner <nicorithner@gmail.com>
2020-09-08 08:21:11 -0600

    Add data to db/seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+StudentCourse.destroy_all
+Course.destroy_all
+Student.destroy_all
+
+
+@student_1 = Student.create!(name: "Student 1", age: 10, house: "House A")
+@student_2 = Student.create!(name: "Student 2", age: 10, house: "House A")
+@student_3 = Student.create!(name: "Student 3", age: 10, house: "House B")
+
+@course_1 = Course.create!(name: "Course 1")
+@course_2 = Course.create!(name: "Course 2")
+@course_3 = Course.create!(name: "Course 3")
+
+StudentCourse.create!(student: @student_1, course: @course_1)
+StudentCourse.create!(student: @student_1, course: @course_2)
+StudentCourse.create!(student: @student_1, course: @course_3)
+
+StudentCourse.create!(student: @student_2, course: @course_1)
+StudentCourse.create!(student: @student_2, course: @course_2)
+StudentCourse.create!(student: @student_2, course: @course_3)
+
+StudentCourse.create!(student: @student_3, course: @course_1)
+StudentCourse.create!(student: @student_3, course: @course_2)
