e2f95f336cedde204a657bf875ab179c46f4a48e
Nico Rithner <nicorithner@gmail.com>
2020-09-08 08:22:26 -0600

    Add show action and strong params

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,4 +2,14 @@ class StudentsController < ApplicationController
   def index
     @students = Student.all
   end
+
+  def show
+    @student = Student.find(student_params[:id])
+  end
+
+  private
+
+  def student_params
+    params.permit(:id, :name, :age, :house, :courses)
+  end
 end
