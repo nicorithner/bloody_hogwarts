8f014307b60108efca5d2c584ea18771dd224258
Nico Rithner <nicorithner@gmail.com>
2020-09-08 16:59:49 -0600

    Add students_average_age method to model

--- app/models/student.rb
+++ app/models/student.rb
@@ -3,4 +3,8 @@ class Student < ApplicationRecord
   has_many :courses, through: :student_courses
 
   validates :name, :age, :house, presence: true
+
+  def self.students_average_ages
+    Student.average(:age)
+  end
 end
